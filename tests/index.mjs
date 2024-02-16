375eef53b92c7994fd4f71d9a517b7a5100cc369
Sean Zellmer <sean@lejeunerenard.com>
2024-02-16 14:04:13 -0600

    chore: Add tests for message contents & bundles

--- tests/index.mjs
+++ tests/index.mjs
@@ -1,5 +1,5 @@
 import test from 'tape'
-import { Client } from 'node-osc'
+import { Bundle, Client } from 'node-osc'
 import { setupTUIO } from '../index.mjs'
 
 test('setupTUIO', (t) => {
@@ -20,7 +20,7 @@ test('setupTUIO', (t) => {
   })
 
   t.test('relays messages to messageport', async (t) => {
-    t.plan(1)
+    t.plan(2)
     const PORT = 3383
     const channel = new MessageChannel()
 
@@ -40,8 +40,48 @@ test('setupTUIO', (t) => {
       resolve()
     }))
 
-    await msgPromise
-    t.pass('message was received')
+    const { data } = await msgPromise
+    t.deepEqual(data, {
+      event: 'tuio:message',
+      message: ['/oscAddress', 200]
+    }, 'received message via MessagePort')
+
+    server.close()
+    channel.port1.close()
+  })
+
+  t.test('relays bundles to messageport', async (t) => {
+    t.plan(4)
+    const PORT = 3383
+    const channel = new MessageChannel()
+
+    const msgPromise = new Promise((resolve) => {
+      channel.port2.onmessage = resolve
+    })
+
+    const [attachMsgPort, server] = setupTUIO(PORT)
+
+    attachMsgPort(channel.port1)
+    channel.port1.start()
+
+    const client = new Client('127.0.0.1', PORT)
+    const bundle = new Bundle(['/oscAddress', 200], ['/bar', 'baz'])
+    await new Promise((resolve, reject) => client.send(bundle, () => {
+      t.pass('was able to send bundle')
+      client.close()
+      resolve()
+    }))
+
+    const { data } = await msgPromise
+    t.equal(data.event, 'tuio:bundle',
+      'received bundle event via MessagePort')
+    t.equal(data.bundle.oscType, 'bundle',
+      'oscType = bundle')
+
+    t.deepEqual(data.bundle.elements, [
+      ['/oscAddress', 200],
+      ['/bar', 'baz']
+    ], 'received elemets via MessagePort')
 
     server.close()
     channel.port1.close()
